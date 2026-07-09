05740e117d652027303f4760f070057bee66253a
DemonHunter4274 <jensonmoult26@gmail.com>
2026-07-09 19:33:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-const WebSocket = require('ws');
+const http = require("http");
+const WebSocket = require("ws");
 
 const port = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port });
+
+// Create a basic HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("JensonAI Online Mode is running ✅");
+});
+
+// Attach WebSocket to the same server
+const wss = new WebSocket.Server({ server });
 
 let lastQuestion = "";
 let lastAnswer = "";
 
-wss.on('connection', ws => {
-    ws.send(JSON.stringify({
-        type: "init",
-        question: lastQuestion,
-        answer: lastAnswer
-    }));
+wss.on("connection", ws => {
+  ws.send(JSON.stringify({
+    type: "init",
+    question: lastQuestion,
+    answer: lastAnswer
+  }));
 
-    ws.on('message', msg => {
-        const data = JSON.parse(msg);
+  ws.on("message", msg => {
+    const data = JSON.parse(msg);
+    if (data.type === "question") lastQuestion = data.text;
+    if (data.type === "answer") lastAnswer = data.text;
 
-        if (data.type === "question") lastQuestion = data.text;
-        if (data.type === "answer") lastAnswer = data.text;
-
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(data));
-            }
-        });
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
     });
+  });
 });
 
-console.log("Server running on port " + port);
+server.listen(port, () => console.log("Server running on port " + port));
+
