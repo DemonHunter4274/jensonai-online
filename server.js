38b9c7e3d943e6944fe6a4a64430a592af4422b4
DemonHunter4274 <jensonmoult26@gmail.com>
2026-07-10 07:03:38 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+let lastQuestion = "";
+let lastAnswer = "";
+
+wss.on('connection', ws => {
+    ws.send(JSON.stringify({
+        type: "init",
+        question: lastQuestion,
+        answer: lastAnswer
+    }));
+
+    ws.on('message', msg => {
+        const data = JSON.parse(msg);
+
+        if (data.type === "question") {
+            lastQuestion = data.text;
+        }
+
+        if (data.type === "answer") {
+            lastAnswer = data.text;
+        }
+
+        // broadcast to all clients
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    });
+});
+
+console.log("JensonAI server running on ws://localhost:8080");
