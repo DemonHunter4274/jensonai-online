fe09d46edf8b61b975d7ddc69f86b30f3e9d6a92
DemonHunter4274 <jensonmoult26@gmail.com>
2026-07-10 07:08:54 +0100

    Integrate HTTP server with WebSocket server
    Refactor server to serve static files and attach WebSocket server.

--- server.js
+++ server.js
@@ -1,35 +1,62 @@
-const WebSocket = require('ws');
+const http = require("http");
+const WebSocket = require("ws");
+const fs = require("fs");
+const path = require("path");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const port = process.env.PORT || 8080;
+
+// Serve static files from /public
+const server = http.createServer((req, res) => {
+  let filePath = "./public" + req.url;
+
+  // Default page
+  if (req.url === "/") filePath = "./public/consumer.html";
+
+  const ext = path.extname(filePath);
+  const contentType =
+    ext === ".html"
+      ? "text/html; charset=utf-8"
+      : "text/plain; charset=utf-8";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content);
+    }
+  });
+});
+
+// Attach WebSocket server to the SAME HTTP server
+const wss = new WebSocket.Server({ server });
 
 let lastQuestion = "";
 let lastAnswer = "";
 
-wss.on('connection', ws => {
-    ws.send(JSON.stringify({
-        type: "init",
-        question: lastQuestion,
-        answer: lastAnswer
-    }));
+wss.on("connection", ws => {
+  ws.send(JSON.stringify({
+    type: "init",
+    question: lastQuestion,
+    answer: lastAnswer
+  }));
 
-    ws.on('message', msg => {
-        const data = JSON.parse(msg);
+  ws.on("message", msg => {
+    const data = JSON.parse(msg);
 
-        if (data.type === "question") {
-            lastQuestion = data.text;
-        }
+    if (data.type === "question") lastQuestion = data.text;
+    if (data.type === "answer") lastAnswer = data.text;
 
-        if (data.type === "answer") {
-            lastAnswer = data.text;
-        }
-
-        // broadcast to all clients
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(data));
-            }
-        });
+    // Broadcast to all clients
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
     });
+  });
 });
 
-console.log("JensonAI server running on ws://localhost:8080");
+server.listen(port, () => {
+  console.log("Server running on port " + port);
+});
